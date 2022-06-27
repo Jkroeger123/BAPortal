c5dc8147aa5d85a5eba1577c5918a1272a13928d
Justin Kroeger <jkroeger75@gmail.com>
2022-06-27 15:03:07 -0400

    init commit

---
+++ components/EmployeeCard.tsx
@@ -0,0 +1,7 @@
+import { Card } from "@mui/material"
+
+const EmployeeCard : React.FC = () => {
+    return <Card></Card>
+}
+
+export default EmployeeCard
---
+++ pages/portal.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import { Typography } from '@mui/material';
+import EmployeeCard from '../components/EmployeeCard';
+
+const Portal: NextPage = () => {
+    return (<div className='w-screen flex jusity-center items-center flex-col'>
+        <div className=''>
+            <Typography variant="h1">Leadership</Typography>
+            
+            {/*Map leadership for API*/}
+
+        </div>
+    </div>);
+}
+
+export default Portal
